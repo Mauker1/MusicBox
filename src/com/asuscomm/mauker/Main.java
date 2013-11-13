e5b1e9af9ef69588e3f23f9ffc9ddd3c3b61836b
Maurício Pessoa <mauricio.ufma@gmail.com>
2013-11-13 00:17:35 -0300

    database creation now working. commiting database example.

--- src/com/asuscomm/mauker/Main.java
+++ src/com/asuscomm/mauker/Main.java
@@ -2,6 +2,7 @@ package com.asuscomm.mauker;
 
 import com.asuscomm.mauker.control.OperationControl;
 import com.asuscomm.mauker.control.OperationControlInterface;
+import com.asuscomm.mauker.db.DBManager;
 import com.asuscomm.mauker.model.MusicBoxModel;
 import com.asuscomm.mauker.model.MusicBoxModelInterface;
 
@@ -10,10 +11,16 @@ import com.asuscomm.mauker.model.MusicBoxModelInterface;
 public class Main {
 	
 	public static void main(String args[]){
+		DBManager.openOrCreateDB();
+		DBManager.createTables();
+		DBManager.closeConnection();
+		
 		MusicBoxModelInterface model = new MusicBoxModel();
 		
 		OperationControlInterface control = new OperationControl(model);
 		
 		control.init();
+		
+		
 	}
 }
--- src/com/asuscomm/mauker/db/DBManager.java
+++ src/com/asuscomm/mauker/db/DBManager.java
@@ -23,9 +23,42 @@ public class DBManager {
     public static void createTables(){
     	try {
 			stmt = c.createStatement();
-			// TODO
-			String sql = "CREATE TABLE "; 
+
+			String sql = "CREATE TABLE IF NOT EXISTS `Musicas` (" +
+					"`idMusica` INTEGER PRIMARY KEY AUTOINCREMENT ," +
+					"`idCategoria` INT NOT NULL ," +
+					"`dsCantor` VARCHAR(45) NULL DEFAULT NULL ," +
+					"`dsMusica` VARCHAR(45) NULL DEFAULT NULL ," +
+					"`vrMusica` VARCHAR(45) NULL DEFAULT NULL ," +
+					"`dsTempo` VARCHAR(45) NULL DEFAULT NULL ," +
+					"`isAtivo` BOOLEAN NULL DEFAULT NULL)";
 	      stmt.executeUpdate(sql);
+	      
+	      sql =  "CREATE TABLE IF NOT EXISTS `Musicas_Categorias` (" +
+	    		     "`idMusicaCategoria` INTEGER PRIMARY KEY AUTOINCREMENT ," +
+	    		     "`dsCategoria` VARCHAR(45) NULL DEFAULT NULL ," +
+	    		     "`isAtivo` BOOLEAN NULL DEFAULT NULL)";
+	      
+	      stmt.execute(sql);
+	      
+	      sql = "CREATE TABLE IF NOT EXISTS `Musicas_Historico` (" +
+	    		     "`idMusicaMoeda` INTEGER PRIMARY KEY AUTOINCREMENT ," +
+	    		     "`idMusica` INTEGER NULL DEFAULT NULL," +
+	    		     "`idMoeda` INTEGER NULL DEFAULT NULL," +
+	    		     "`qtdTroco` FLOAT(8,2) NULL DEFAULT NULL ," +
+	    		     "`dtMusica` TIMESTAMP NULL DEFAULT NULL ," +
+	    		     "`isAtivo` BOOLEAN NULL DEFAULT NULL)";
+	      
+	      stmt.execute(sql);
+	      
+	      sql =  "CREATE TABLE IF NOT EXISTS `Moedas` (" +
+	    		   "`idMoeda` INTEGER PRIMARY KEY AUTOINCREMENT ," +
+	    		   "`tpMoeda` VARCHAR(45) NULL DEFAULT NULL ," +
+	    		   "`vrMoeda` FLOAT(8,2) NULL DEFAULT NULL ," +
+	    		   "`isAtivo` BOOLEAN NULL DEFAULT NULL)";
+	      
+	      stmt.execute(sql);
+	      
 	      stmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
